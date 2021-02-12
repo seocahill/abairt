92f9930a26aeabc8e59af53b9ccabeef4b13ba46
Seosamh Cahill <seo.cahill@gmail.com>
2021-02-12 21:57:20 +0100

    Works

---
+++ app/assets/javascripts/controllers/recorder_controller.js
@@ -0,0 +1,63 @@
+import { Controller } from "stimulus"
+// import { DirectUpload } from "@rails/activestorage"
+import { DirectUpload } from 'https://cdn.skypack.dev/@rails/activestorage';
+
+export default class extends Controller {
+  static targets = ["input", "recordButton", "stopButton"]
+
+  connect() {
+    this.stopButtonTarget.classList.toggle('hidden')
+  }
+
+  record() {
+    this.chunks = []
+    this.stream().then(stream => {
+      this._stream = stream
+      this.flipButtons()
+      this.recorder().start(1000)
+    })
+  }
+
+  stop() {
+    this.flipButtons()
+    this.recorder().stop()
+  }
+
+  stream() {
+    if (this._stream == undefined) {
+      return navigator.mediaDevices.getUserMedia({ audio: true })
+    }
+    return this._stream
+  }
+
+  recorder() {
+    if (this._recorder == undefined) {
+      let that = this
+      this._recorder = new MediaRecorder(this.stream())
+      this._recorder.ondataavailable = function (e) {
+        that.chunks.push(e.data)
+      }
+      this._recorder.onstop = function (e) {
+        const file = new File(that.chunks, 'audio.ogg', { type: 'audio/ogg' })
+        const upload = new DirectUpload(file, that.url())
+        upload.create((error, blob) => {
+          that.hiddenInput = document.createElement('input')
+          that.hiddenInput.type = 'hidden'
+          that.hiddenInput.name = that.inputTarget.name
+          that.hiddenInput.value = blob.signed_id
+          that.inputTarget.parentNode.insertBefore(that.hiddenInput, that.inputTarget.nextSibling)
+        })
+      }
+    }
+    return this._recorder
+  }
+
+  flipButtons() {
+    this.recordButtonTarget.classList.toggle('hidden')
+    this.stopButtonTarget.classList.toggle('hidden')
+  }
+
+  url() {
+    return this.inputTarget.getAttribute('data-direct-upload-url')
+  }
+}
--- app/models/dictionary_entry.rb
+++ app/models/dictionary_entry.rb
@@ -1,2 +1,3 @@
 class DictionaryEntry < ApplicationRecord
+  has_one_attached :media
 end
