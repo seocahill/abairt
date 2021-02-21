29aac3234c2e203c5c41fb3e2e7705cc89d23565
Seosamh Cahill <seo.cahill@gmail.com>
2021-02-21 15:20:18 +0100

    working logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,12 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_path
     else
-      raise
+      render :new
     end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   resources :dictionary_entries
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'dictionary_entries#index'
