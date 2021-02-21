9e86f52b2f6f612cb0ad7a06bef14aaebacf396a
Seosamh Cahill <seo.cahill@gmail.com>
2021-02-21 15:53:58 +0100

    add confirmed boolean

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    User.find_by(id: session[:user_id])
+    User.find_by(id: session[:user_id], confirmed: true)
   end
 end
