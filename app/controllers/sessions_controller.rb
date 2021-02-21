00904976032164c302f95736ffa8463fc274f1fa
Seosamh Cahill <seo.cahill@gmail.com>
2021-02-21 13:58:06 +0100

    Somewhat working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
+  def new; end
+
   def create
-    @user = User.create(params.require(:user).permit(:username, :password))
-    session[:user_id] = @user.id
-    redirect_to @user
+    user = User.find_by(email: params[:email])
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      raise
+    end
   end
 end
