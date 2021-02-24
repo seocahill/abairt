fe278cc2268c7f7c92b5e1a0b1d529265a8c851c
Seosamh Cahill <seo.cahill@gmail.com>
2021-02-24 17:46:50 +0100

    Rang creation working

--- app/controllers/rangs_controller.rb
+++ app/controllers/rangs_controller.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 class RangsController < ApplicationController
-  before_action :set_rang, only: %i[ show edit update destroy ]
+  before_action :set_rang, only: %i[show edit update destroy]
+  before_action :authorize, except: %i[show]
 
   # GET /rangs or /rangs.json
   def index
-    @rangs = Rang.all
+    @rangs = current_user.rangs.all
   end
 
   # GET /rangs/1 or /rangs/1.json
-  def show
-  end
+  def show; end
 
   # GET /rangs/new
   def new
@@ -16,16 +18,15 @@ class RangsController < ApplicationController
   end
 
   # GET /rangs/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /rangs or /rangs.json
   def create
-    @rang = Rang.new(rang_params)
+    @rang = Rang.new(rang_params.merge(user_id: current_user.id))
 
     respond_to do |format|
       if @rang.save
-        format.html { redirect_to @rang, notice: "Rang was successfully created." }
+        format.html { redirect_to @rang, notice: 'Rang was successfully created.' }
         format.json { render :show, status: :created, location: @rang }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +39,7 @@ class RangsController < ApplicationController
   def update
     respond_to do |format|
       if @rang.update(rang_params)
-        format.html { redirect_to @rang, notice: "Rang was successfully updated." }
+        format.html { redirect_to @rang, notice: 'Rang was successfully updated.' }
         format.json { render :show, status: :ok, location: @rang }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -51,19 +52,26 @@ class RangsController < ApplicationController
   def destroy
     @rang.destroy
     respond_to do |format|
-      format.html { redirect_to rangs_url, notice: "Rang was successfully destroyed." }
+      format.html { redirect_to rangs_url, notice: 'Rang was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_rang
-      @rang = Rang.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def rang_params
-      params.require(:rang).permit(:name)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_rang
+    @rang = current_user.rangs.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def rang_params
+    params.require(:rang).permit(:name, :user_id)
+  end
+
+  def authorize
+    return if current_user
+
+    redirect_to root_path
+  end
 end
