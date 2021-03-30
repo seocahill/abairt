fd4db558a5c652c1fdfbd6bd7283e85a917b9051
Seosamh Cahill <seo.cahill@gmail.com>
2021-03-30 19:57:48 +0200

    Add jitsi

--- app/assets/javascripts/controllers/rang_controller.js
+++ app/assets/javascripts/controllers/rang_controller.js
@@ -3,9 +3,23 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = ["cell"]
 
+  connect() {
+    this.startMeeting()
+  }
+
   hide() {
     this.cellTargets.forEach((cellTarget) => {
       cellTarget.classList.toggle("hidden")
     })
+  }
+
+  startMeeting() {
+    const domain = 'meet.jit.si';
+    const options = {
+      roomName: `abairt-${Math.random().toString(16).substr(2, 8)}`,
+      height: 700,
+      parentNode: document.querySelector('#meet')
+    };
+    const api = new JitsiMeetExternalAPI(domain, options);
   }
 }
