23eca50a11c759f21c8c0c9b8252d0013f9d0b4f
Seosamh Cahill <seo.cahill@gmail.com>
2021-04-05 00:34:59 +0200

    Better player

--- app/assets/javascripts/controllers/rang_controller.js
+++ app/assets/javascripts/controllers/rang_controller.js
@@ -1,17 +1,39 @@
 import { Controller } from "stimulus"
+import WaveSurfer from 'https://cdn.skypack.dev/wavesurfer.js';
 
 export default class extends Controller {
   static targets = ["cell"]
-  static values = { meetingId: String }
+  static values = { meetingId: String, media: String }
 
-  initialize() {
-    console.log("meeting id is", this.meetingIdValue)
+  connect() {
+    if (this.mediaValue) {
+      this.waveSurfer = WaveSurfer.create({
+        container: '#waveform',
+        waveColor: 'violet',
+        progressColor: 'purple'
+      })
+      this.waveSurfer.load(this.mediaValue);
+    }
   }
 
   hide() {
     this.cellTargets.forEach((cellTarget) => {
       cellTarget.classList.toggle("hidden")
     })
+  }
+
+  play(event) {
+    event.preventDefault()
+    this.waveSurfer.playPause()
+  }
+
+  loadWave() {
+    let ws = WaveSurfer.create({
+      container: '#waveform',
+      waveColor: 'violet',
+      progressColor: 'purple'
+    })
+    ws.load(this.mediaValue);
   }
 
   startMeeting() {
