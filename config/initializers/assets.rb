9d56e4c5b579f4329516b7ff015dd2a3d2fe4c7a
Seosamh Cahill <seo.cahill@gmail.com>
2021-04-05 11:27:40 +0200

    Fix pagination styling
    allow custom tailwind styles

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,4 @@ Rails.application.config.assets.precompile += %w(application.js)
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Sprockets.register_compressor "text/css", :custom_purger, Tailwindcss::Compressor.new({ only_purge: %w[tailwind_custom] })
