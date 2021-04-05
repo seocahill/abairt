1faf09199e59e79aacdfc0f6f6ba16b7bf849508
Seosamh Cahill <seo.cahill@gmail.com>
2021-04-05 10:53:13 +0200

    Fix url generation

--- app/models/dictionary_entry.rb
+++ app/models/dictionary_entry.rb
@@ -1,17 +1,18 @@
 # frozen_string_literal: true
 
-require 'csv'
+require "csv"
+
 class DictionaryEntry < ApplicationRecord
   has_one_attached :media
   has_many :rang_entries, dependent: :destroy
   has_many :rangs, through: :rang_entries
 
-  after_create_commit { broadcast_prepend_to 'dictionary_entries' }
-  after_update_commit { broadcast_replace_later_to 'dictionary_entries' }
-  after_destroy_commit { broadcast_remove_to 'dictionary_entries' }
+  after_create_commit { broadcast_prepend_to "dictionary_entries" }
+  after_update_commit { broadcast_replace_later_to "dictionary_entries" }
+  after_destroy_commit { broadcast_remove_to "dictionary_entries" }
 
   include PgSearch::Model
-  pg_search_scope :search_translation, against: :translation, using: { tsearch: { dictionary: 'english' } }
+  pg_search_scope :search_translation, against: :translation, using: { tsearch: { dictionary: "english" } }
 
   validates :word_or_phrase, uniqueness: { case_sensitive: false }, allow_blank: true
 
@@ -30,8 +31,8 @@ class DictionaryEntry < ApplicationRecord
   end
 
   def media_url
-    return '' unless media.attached?
+    return "" unless media.attached?
 
-    Rails.application.routes.url_helpers.rails_blob_path(media, only_path: true)
+    Rails.application.routes.url_helpers.url_for(media)
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
-require 'active_support/core_ext/integer/time'
+require "active_support/core_ext/integer/time"
+
+Rails.application.default_url_options = { host: "abairt.com" }
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -15,7 +17,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
@@ -24,7 +26,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
   # Compress CSS using a preprocessor.
   config.assets.css_compressor = :purger
@@ -90,10 +92,10 @@ Rails.application.configure do
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  if ENV['RAILS_LOG_TO_STDOUT'].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
   # Do not dump schema after migrations.
