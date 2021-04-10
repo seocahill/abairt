03bba11e31c836000767b8d08b0e2c85b971a970
Seosamh Cahill <seo.cahill@gmail.com>
2021-04-10 11:48:09 +0200

    Fix remove card

--- app/controllers/ceist_controller.rb
+++ app/controllers/ceist_controller.rb
@@ -17,14 +17,15 @@ class CeistController < ApplicationController
       .joins(:rangs)
       .where(rangs: { user_id: current_user.id })
       .where(committed_to_memory: false)
+      .where("translation is not null")
       .where("date_trunc('day', recall_date) = date_trunc('day', current_date) OR recall_date IS NULL")
       .take
   end
 
   def create
     dictionary_entry = DictionaryEntry.find(params[:entry_id])
-    if params[:quality_of_recall].to_i == 5 && dictionary_entry.recall_date.nil?
-      dictionary_entry.update(committed_to_memory: true)
+    if params[:quality_of_recall].nil?
+      dictionary_entry.update!(committed_to_memory: true)
     else
       super_memo = SuperMemoService.new(params[:quality_of_recall].to_i, dictionary_entry.previous_inteval, dictionary_entry.previous_easiness_factor)
       dictionary_entry.update(
