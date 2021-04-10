9a8f6168d17d1241507937559383268d063b5161
Seosamh Cahill <seo.cahill@gmail.com>
2021-04-10 10:46:52 +0200

    Improve rang

--- app/helpers/rangs_helper.rb
+++ app/helpers/rangs_helper.rb
@@ -4,4 +4,14 @@ module RangsHelper
   def current_user_can_edit
     current_user&.rangs&.include?(@rang)
   end
+
+  def type(rang)
+    if rang.url
+      ["Alt", "green"]
+    elsif rang.media.attached?
+      ["Tras-scríbhinn", "purple"]
+    else
+      ["Comhrá", "gray"]
+    end
+  end
 end
