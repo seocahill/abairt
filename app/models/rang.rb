33863c6a47e49214c61d2d3a76c23c575fa0be14
Seosamh Cahill <seo.cahill@gmail.com>
2021-04-11 12:58:40 +0200

    Add notifications

--- app/models/rang.rb
+++ app/models/rang.rb
@@ -6,14 +6,33 @@ class Rang < ApplicationRecord
   belongs_to :user
 
   before_create :generate_meeting_id
+  after_commit :send_notification
 
   has_one_attached :media
 
-  # private
+  def next_time
+    return  "aon am" unless time
+    return "críochnaithe" unless time > Time.now
+
+    time.strftime("%b %e, %l:%M %p %Z")
+  end
+
+  def participants
+    ([user.email] + user.daltaí.pluck(:email) + [user.máistir&.email]).compact
+  end
+
+  private
 
   def generate_meeting_id
     begin
       self.meeting_id = SecureRandom.hex
     end while self.class.exists?(meeting_id: meeting_id)
+  end
+
+  def send_notification
+    return unless time
+    return time > Time.now
+
+     NotificationsMailer.with(rang: self).ceád_rang_eile
   end
 end
