0da269ab57f4b33648bc2193276d37a71dfdd6e8
Seosamh Cahill <seo.cahill@gmail.com>
2021-04-11 16:27:24 +0200

    from verified user

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,5 +1,5 @@
 class NotificationsMailer < ApplicationMailer
-  default from: 'notifications@abairt.com'
+  default from: 'abairt@abairt.com'
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
