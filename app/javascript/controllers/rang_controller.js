3a775b97328f6d626408324eccd3db913683c84b
Seosamh Cahill <seo.cahill@gmail.com>
2022-08-05 20:17:42 +0200

    drag selection working anyway

--- app/javascript/controllers/rang_controller.js
+++ app/javascript/controllers/rang_controller.js
@@ -29,34 +29,38 @@ export default class extends Controller {
     let that = this;
     this.waveSurfer = WaveSurfer.create({
       container: '#waveform',
-      waveColor: 'violet',
-      progressColor: 'purple',
+      // waveColor: 'violet',
+      // progressColor: 'purple',
       partialRender: true,
+      pixelRatio: 1,
+      scrollParent: true,
+      // normalize: true,
       plugins: [
         RegionsPlugin.create({
-          // plugin options ...
+          dragSelection: true,
         })
       ]
     })
     this.waveSurfer.load(this.mediaValue);
-    this.waveSurfer.on('loading', function (progress) {
-      if (progress && progress < 99) {
-        playButton.innerHTML = `loading ${progress}%`;
-      } else {
-        playButton.innerHTML = "Preparing wave....";
-      }
-    })
+    // this.waveSurfer.on('loading', function (progress) {
+    //   if (progress && progress < 99) {
+    //     playButton.innerHTML = `loading ${progress}%`;
+    //   } else {
+    //     playButton.innerHTML = "Preparing wave....";
+    //   }
+    // })
     this.waveSurfer.on('ready', function() {
-      playButton.innerHTML = "Play / Pause"
+      // playButton.innerHTML = "Play / Pause";
+      // that.waveSurfer.enableDragSelection();
     })
-    this.waveSurfer.on('audioprocess', function () {
-      if (that.waveSurfer.isPlaying()) {
-        that.timeTarget.innerText = that.waveSurfer.getCurrentTime().toFixed(1)
-      }
-    })
-    this.waveSurfer.on('seek', function() {
-      that.timeTarget.innerText = that.waveSurfer.getCurrentTime().toFixed(1)
-    })
+    // this.waveSurfer.on('audioprocess', function () {
+    //   if (that.waveSurfer.isPlaying()) {
+    //     that.timeTarget.innerText = that.waveSurfer.getCurrentTime().toFixed(1)
+    //   }
+    // })
+    // this.waveSurfer.on('seek', function() {
+    //   that.timeTarget.innerText = that.waveSurfer.getCurrentTime().toFixed(1)
+    // })
   }
 
   wordSearchTargetConnected() {
