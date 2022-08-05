bb4e4e7a547b50be4a9f1bb7af756e3677681fd1
Seosamh Cahill <seo.cahill@gmail.com>
2022-08-05 18:21:41 +0200

    Add plugin

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,6 +9,7 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.12/nodelibs/browser/process-production.js"
 pin "util", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.12/nodelibs/browser/util.js"
 pin "wavesurferjs", to: "https://ga.jspm.io/npm:wavesurfer@1.3.4/dist/wavesurfer.min.js"
+pin "wavesurferregionsjs", to: "https://ga.jspm.io/npm:wavesurfer@1.3.4/dist/plugin/wavesurfer.regions.min.js"
 pin "@rails/activestorage", to: "https://ga.jspm.io/npm:@rails/activestorage@6.1.4-1/app/assets/javascripts/activestorage.js"
 pin "@rails/actioncable", to: "https://ga.jspm.io/npm:@rails/actioncable@6.1.4-1/app/assets/javascripts/action_cable.js"
 pin "jitsi" # @2.1.5
