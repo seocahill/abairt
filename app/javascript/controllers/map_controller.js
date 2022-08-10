4c302dd347c19dfb9567acbee05a37bd90bfdff4
Seosamh Cahill <seo.cahill@gmail.com>
2022-08-10 23:01:59 +0200

    Comhra create working

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,15 +1,24 @@
 import { Controller } from "@hotwired/stimulus"
 import L from 'leaflet'
 
-
 export default class extends Controller {
   connect() {
-    let map = L.map('map').setView([53.9110, -9.4527], 9);
+    this.map = L.map('map').setView([53.9110, -9.4527], 9);
+
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '© OpenStreetMap'
-    }).addTo(map);
-    let marker = L.marker([53.91, -9.4527]).addTo(map);
-    map.on('click', (e) => { L.popup().setLatLng(e.latlng).setContent("You clicked the map at " + e.latlng.toString()).openOn(map); });
+    }).addTo(this.map);
+
+    // let marker = L.marker([53.91, -9.4527]).addTo(map);
+    this.map.on('click', (e) => { this.showLatLang(e) });
+  }
+
+  showLatLang(e) {
+    if (this.marker) {
+      this.marker.remove()
+    }
+    this.marker = L.marker(e.latlng).addTo(this.map);
+    document.getElementById("comhra_lat_lang").value = `${e.latlng.lat},${e.latlng.lng}`;
   }
 }
