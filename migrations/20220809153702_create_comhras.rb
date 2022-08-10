cf07c7810bbf32c9c58d970cfa201a1c4972718e
Seosamh Cahill <seo.cahill@gmail.com>
2022-08-10 22:00:59 +0200

    basis comhra index

--- migrations/20220809153702_create_comhras.rb
+++ migrations/20220809153702_create_comhras.rb
@@ -1,6 +1,10 @@
 class CreateComhras < ActiveRecord::Migration[6.1]
   def change
     create_table :comhras do |t|
+      t.string :name
+      t.string :lat_lang
+      t.references :user, null: false, foreign_key: true
+      t.references :grupa, null: false, foreign_key: true
 
       t.timestamps
     end
