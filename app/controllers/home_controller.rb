400e417b5ae647163af669836d0a471efe589f80
Seosamh Cahill <seo.cahill@gmail.com>
2023-05-05 00:16:36 +0200

    fix: homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @grupai = Grupa.all
-    @pins = @grupai.where.not(lat_lang: nil).joins(:rangs).map do |g|
+    @pins = User.where.not(lat_lang: nil).map do |g|
       g.slice(:id, :ainm, :lat_lang).tap do |c|
         if (sample = g.rangs.detect { |r| r.media.audio? }&.media)
           c[:media_url] = Rails.application.routes.url_helpers.rails_blob_url(sample)
