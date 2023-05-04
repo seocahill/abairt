d54108746c26203a96ba0c131109f795628d7614
Seosamh Cahill <seo.cahill@gmail.com>
2023-05-05 00:42:32 +0200

    dummy faq

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
+  Faq = Struct.new(:title, :body)
   def faq
+    @faqs = [
+      Faq.new("test", "lorem"),
+      Faq.new("test2", "lorem2")
+    ]
   end
 end
