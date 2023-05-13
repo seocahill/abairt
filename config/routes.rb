9e2fc9ca26bd9b2493fe00815f72fe6c669c73ea
Seosamh Cahill <seo.cahill@gmail.com>
2023-05-13 22:11:13 +0200

    feat: add lists controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :lists
   resources :voice_recordings do
     resources :dictionary_entries, only: :create, module: :voice_recordings
     member { get :preview }
