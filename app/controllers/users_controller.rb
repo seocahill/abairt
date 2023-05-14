d7eea9a2440cef9b0c422b973af1a0add322e537
Seosamh Cahill <seo.cahill@gmail.com>
2023-05-15 01:10:38 +0200

    feat: user map view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,14 @@ class UsersController < ApplicationController
       records = records.joins(:fts_users).where("fts_users match ?", params[:search]).distinct.order('rank')
     end
 
+    @pins = User.where.not(lat_lang: nil).map do |g|
+      g.slice(:id, :name, :lat_lang).tap do |c|
+        if (sample = g.rangs.detect { |r| r.media.audio? }&.media)
+          c[:media_url] = Rails.application.routes.url_helpers.rails_blob_url(sample)
+        end
+      end
+    end
+
     @new_speaker = User.new
 
     @pagy, @users = pagy(records, items: 12)
