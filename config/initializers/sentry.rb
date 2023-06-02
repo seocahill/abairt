3d1256ad55f228ad7cff47ee328cee5db45ddca7
Seosamh Cahill <seo.cahill@gmail.com>
2023-06-03 01:30:05 +0200

    fix: remove old sentry key (rotated)

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Sentry.init do |config|
-  config.dsn = 'https://f80c229494bd45f4bd1522bebbe318c3@o353348.ingest.sentry.io/5656899'
   config.breadcrumbs_logger = [:active_support_logger]
 
   # To activate performance monitoring, set one of these options.
