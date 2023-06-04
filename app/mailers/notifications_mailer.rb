e086257ae39ec7953ad253b19dd76cb2ed45b0fa
Seosamh Cahill <seo.cahill@gmail.com>
2023-06-04 20:29:59 +0200

    fix: working email setup

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,5 +1,5 @@
 class NotificationsMailer < ApplicationMailer
-  default from: 'abairt@abairt.com'
+  default from: 'info@abairt.com'
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
