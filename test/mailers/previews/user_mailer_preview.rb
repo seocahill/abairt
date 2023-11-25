26278c33a6d42bec370b531846c45c3c1d6703d8
Seosamh Cahill <seo.cahill@gmail.com>
2023-11-26 00:40:06 +0100

    fix: chatgpt bug

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -3,12 +3,14 @@ class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset_email
   def password_reset_email
-    UserMailer.password_reset_email(User.first)
+    user = User.order(updated_at: :desc).first
+    UserMailer.password_reset_email(user)
   end
 
    # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset_email
   def new_user_email
-    UserMailer.new_user_email(User.last)
+    user = User.order(updated_at: :desc).first
+    UserMailer.new_user_email(user)
   end
 
 end
