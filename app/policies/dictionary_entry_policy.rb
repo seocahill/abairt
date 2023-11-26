c6a9d4f054d79de84ac3d9341e8e5ac8d3c66230
Seosamh Cahill <seo.cahill@gmail.com>
2023-11-26 01:22:27 +0100

    fix: various permissions issues

--- app/policies/dictionary_entry_policy.rb
+++ app/policies/dictionary_entry_policy.rb
@@ -4,6 +4,10 @@ class DictionaryEntryPolicy < ApplicationPolicy
     true if user == record.owner
   end
 
+  def update?
+    true if user && !user.student?
+  end
+
   def destroy?
     # only owners can destroy dictionary entries
     true if user == record.owner
--- app/policies/rang_policy.rb
+++ app/policies/rang_policy.rb
@@ -3,6 +3,18 @@ class RangPolicy < ApplicationPolicy
     true if user
   end
 
+  def create?
+    true if user.teacher? || user.admin?
+  end
+
+  def edit?
+    true if record.teacher == user
+  end
+
+  def destroy?
+    true if record.teacher == user
+  end
+
   def show?
     true if user
   end
