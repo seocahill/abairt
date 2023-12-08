ae4fa9d0741b8560e430e589b5324cd1f889bc53
Seosamh Cahill <seo.cahill@gmail.com>
2023-12-09 00:41:20 +0100

    fix: update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+    origins 'abairt.com'
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :delete, :options, :head],
+      credentials: false
   end
 end
