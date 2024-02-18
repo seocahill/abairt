6bed0bec60794648ed60f25b561ff0aeb8266cc9
Seosamh Cahill <seo.cahill@gmail.com>
2024-02-18 01:33:01 +0000

    feat: working version

--- app/jobs/import_transcription_job.rb
+++ app/jobs/import_transcription_job.rb
@@ -2,31 +2,13 @@ class ImportTranscriptionJob < ApplicationJob
   queue_as :default
 
   def perform(voice_recording)
-     @llm = Langchain::LLM::OpenAI.new(api_key: Rails.application.credentials.dig(:openai, :openai_key),  default_options: {
+     llm = Langchain::LLM::OpenAI.new(api_key: Rails.application.credentials.dig(:openai, :openai_key),  default_options: {
       chat_completion_model_name: "gpt-4-1106-preview", completion_model_name: "gpt-4-1106-preview"
     })
-    schema = {
-      "$schema": "http://json-schema.org/draft-07/schema#",
-      "type": "array",
-      "items": {
-        "type": "object",
-        "properties": {
-          "word_or_phrase": {
-            "type": "string"
-          },
-          "translation": {
-            "type": "string"
-          }
-        },
-        "required": ["word_or_phrase", "translation"]
-      }
-    }
-    parser = Langchain::OutputParsers::StructuredOutputParser.from_json_schema(json_schema)
-    prompt = Langchain::Prompt::PromptTemplate.new(template: "Take the following text and split into phrases.  Return each phrase with an english translation.\n{format_instructions}\nIrish Text: {text}", input_variables: ["text", "format_instructions"])
-    prompt_text = prompt.format(description: voice_recording.transcription, format_instructions: parser.get_format_instructions)
-    llm_response = llm.chat(prompt: prompt_text).completion
-    parser.parse(llm_response).dig('items').each do |item|
-      voice_recording.dictionary_entries.create(word_or_phrase: item['word_or_phrase'], translation: item['translation'], user_id: voice_recording.user_id)
+    voice_recording.transcription.split(/\.|\?|!/).each do |text|
+      prompt_text = "translate this piece of irish text: '#{text}' into english. return the translation only, no other information is required. q"
+      llm_response = llm.chat(prompt: prompt_text).completion
+      voice_recording.dictionary_entries.create!(word_or_phrase: text, translation: llm_response, user_id: voice_recording.user_id)
     end
   end
 end
