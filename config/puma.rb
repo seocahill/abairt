1615050f5e59b16bd42dbdb0a5c3440f84fb5311
Seosamh Cahill <seo.cahill@gmail.com>
2024-06-04 22:36:23 +0100

    fix: fork puma

--- config/puma.rb
+++ config/puma.rb
@@ -30,14 +30,17 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
-# preload_app!
+preload_app!
+# on_worker_boot do
+  # redis maybe?
+# end
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
