c3181eded5883a371a3ccb954cbe8d8c8496cd4c
Seosamh Cahill <seo.cahill@gmail.com>
2024-06-07 00:54:55 +0100

    docs: deps

--- test/jobs/generate_peaks_job_test.rb
+++ test/jobs/generate_peaks_job_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class GeneratePeaksJobTest < ActiveJob::TestCase
   test "that peaks are generated" do
     assert voice_recordings(:one).media.attached?
+    # note will fail unless you have ffmpeg and audiowaveform installed
     GeneratePeaksJob.perform_now(voice_recordings(:one).id)
     assert voice_recordings(:one).reload.peaks.length > 1
   end
