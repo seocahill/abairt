ba60b1499c270eeb140b3bdb07f5259dbd9b264d
Seosamh Cahill <seo.cahill@gmail.com>
2024-08-23 00:02:37 +0100

    fix autotag

--- app/models/dictionary_entry.rb
+++ app/models/dictionary_entry.rb
@@ -127,12 +127,12 @@ class DictionaryEntry < ApplicationRecord
     client = OpenAI::Client.new(access_token: Rails.application.credentials.dig(:openai, :openai_key),
       organization_id: Rails.application.credentials.dig(:openai, :openai_org))
 
-    context =  "You are an Irish language translator. You take words and phrases and tag them using the following schema: Abstract nouns; Activities; Adjectives; Adverbs; Amount; Animals; Arguments; Astronomy; Buildings; Calendar & Seasons; Clothes; Colours; Comparatives; Conversation; Countryside; Disaster; Everyday phrases; Facial expressions; Farming life ; Feelings; Folklore; Food and drink; Games; Geographical terms; Grammar; Greetings; Health; Idioms; Interjections; Language; Life & death; Likes & dislikes; Measurement; Money; Music; Noise and sounds; Numbers; Objects; Past participle; People; Personality; Physical contact; Physical descriptions; Place names; Plants; Prepositions; Pronouns; Proverbs; Reduplication; Relationships; Religion; Rest and relaxation; School; Seashore wildlife; Shapes; Similes; Smells; Terms of endearment; The body; The city; The family; The home; The seashore; Timber; Time; To be able to; Toys; Transport; Verbs; Weather; Work. You choose as many tags from this schema as is appropriate."
+    context =  "You are an Irish language translator. You take words and phrases and tag them using the following schema: Abstract nouns; Activities; Adjectives; Adverbs; Amount; Animals; Arguments; Astronomy; Buildings; Calendar & Seasons; Clothes; Colours; Comparatives; Conversation; Countryside; Disaster; Everyday phrases; Facial expressions; Farming life ; Feelings; Folklore; Food and drink; Games; Geographical terms; Grammar; Greetings; Health; Idioms; Interjections; Language; Life & death; Likes & dislikes; Measurement; Money; Music; Noise and sounds; Numbers; Objects; Past participle; People; Personality; Physical contact; Physical descriptions; Place names; Plants; Prepositions; Pronouns; Proverbs; Reduplication; Relationships; Religion; Rest and relaxation; School; Seashore wildlife; Shapes; Similes; Smells; Terms of endearment; The body; The city; The family; The home; The seashore; Timber; Time; To be able to; Toys; Transport; Verbs; Weather; Work. Choose up to five appropriate tags."
 
     prompt = "Please analyze the following phrase and generate tags for it. The results you return should be a single array of the tags you have chosen, nothing else. Phrase: #{translation}"
 
     response = client.chat(parameters: {
-      model: "gpt-4o",  # or use the appropriate model you have access to
+      model: "gpt-4o-mini",  # or use the appropriate model you have access to
       messages: [
         { "role": "system", "content": context },
         { "role": "user", "content": prompt }
