9271d582e8798488ac4b590a0c2a3999747e8ec0
Seosamh Cahill <seo.cahill@gmail.com>
2025-01-27 11:07:14 +0100

    fix: remove Turbo Stream handling in diarization controller
    - Removed Turbo Stream response rendering in DiarizationsController
    - Disabled Turbo confirmation for diarization button in waveform view

--- app/controllers/diarizations_controller.rb
+++ app/controllers/diarizations_controller.rb
@@ -7,12 +7,10 @@ class DiarizationsController < ApplicationController
     if service.diarize
       respond_to do |format|
         format.html { redirect_to @voice_recording, notice: 'Diarization started successfully. This may take a few minutes depending on the length of the recording. You can refresh this page to see the status.' }
-        format.turbo_stream { render turbo_stream: turbo_stream.append('flash', partial: 'shared/flash', locals: { notice: 'Diarization started successfully. This may take a few minutes depending on the length of the recording. You can refresh this page to see the status.' }) }
       end
     else
       respond_to do |format|
         format.html { redirect_to @voice_recording, alert: 'Failed to start diarization.' }
-        format.turbo_stream { render turbo_stream: turbo_stream.append('flash', partial: 'shared/flash', locals: { alert: 'Failed to start diarization.' }) }
       end
     end
   end
