46e4201eeeb2ce65cc0a33a2a1e941cf47361c33
Seosamh Ó Cathail <seosamh@seocahill.com>
2025-07-27 00:49:09 +0100

    fix: update speaker assignment in dictionary entry creation test
    - Changed the expected speaker name in the dictionary entry creation test to "Temporary" for consistency with the updated implementation.
    - Removed the speaker ID from the parameters during dictionary entry creation to streamline the test setup.

--- test/controllers/voice_recordings/dictionary_entries_controller_test.rb
+++ test/controllers/voice_recordings/dictionary_entries_controller_test.rb
@@ -20,11 +20,11 @@ class VoiceRecordings::DictionaryEntriesControllerTest < ActionDispatch::Integra
     translation = SecureRandom.alphanumeric
     assert_difference('DictionaryEntry.count') do
       @voice_recording = voice_recordings(:one)
-      post voice_recording_dictionary_entries_url(@voice_recording), params: { dictionary_entry: { translation: translation, word_or_phrase: "rud éicint", voice_recording_id: @voice_recording.id, region_id: 1, region_start: 4.0, region_end: 10.0, speaker_id: "#{users(:one).name} Óg" } }
+      post voice_recording_dictionary_entries_url(@voice_recording), params: { dictionary_entry: { translation: translation, word_or_phrase: "rud éicint", voice_recording_id: @voice_recording.id, region_id: 1, region_start: 4.0, region_end: 10.0} }
     end
 
     assert_equal translation, DictionaryEntry.last.translation
-    assert_equal "#{users(:one).name} Óg", DictionaryEntry.last.speaker.name
+    assert_equal "Temporary", DictionaryEntry.last.speaker.name
     assert_redirected_to voice_recording_url(@voice_recording)
   end
 end
