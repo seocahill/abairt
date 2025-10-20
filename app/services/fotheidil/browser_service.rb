3077d998b09dfba42baba93ed4a4aa80053294f9
Seosamh Ó Cathail <seosamh@seocahill.com>
2025-10-21 00:55:30 +0200

    chore: update Docker image version and enhance Selenium browser options
    - Updated the Docker image version for the production environment to a new commit.
    - Modified the Selenium browser setup to allow headful mode when the `SELENIUM_HEADFUL` environment variable is set to true, improving flexibility for testing environments.

--- app/services/fotheidil/browser_service.rb
+++ app/services/fotheidil/browser_service.rb
@@ -17,7 +17,7 @@ module Fotheidil
 
     def setup_browser
       options = Selenium::WebDriver::Chrome::Options.new
-      options.add_argument("--headless") unless Rails.env.development?
+      options.add_argument("--headless") unless Rails.env.development? || ENV["SELENIUM_HEADFUL"] == "true"
       options.add_argument("--no-sandbox")
       options.add_argument("--disable-dev-shm-usage")
       options.add_argument("--disable-gpu")
