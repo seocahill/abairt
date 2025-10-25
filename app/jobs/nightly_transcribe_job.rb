ba0605c8c5b3ccb30b31a989adac31bf0223be92
Seosamh Ó Cathail <seosamh@seocahill.com>
2025-10-25 10:47:14 +0200

    chore: remove rufus-scheduler dependency and simplify nightly transcription job logic

--- app/jobs/nightly_transcribe_job.rb
+++ app/jobs/nightly_transcribe_job.rb
@@ -2,14 +2,7 @@ class NightlyTranscribeJob < ApplicationJob
   queue_as :default
 
   def perform
-    # Find the last voice recording that hasn't been diarized yet
-    voice_recording = VoiceRecording
-      .joins(:media_attachment)
-      .where(diarization_status: [nil, 'not_started', 'failed'])
-      .order(created_at: :desc)
-      .first
-
-    voice_recording ||= VoiceRecording.import_from_archive
+    voice_recording = VoiceRecording.import_from_archive
 
     return unless voice_recording
 
