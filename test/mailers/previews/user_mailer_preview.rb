53ec688b6959deb2bc5070bf2d87480c3e1e4617
Seosamh Ó Cathail <seosamh@seocahill.com>
2025-12-26 22:02:32 +0100

    refactor: update login email functionality and form action
    - Changed the form action in the login view from password resets to login requests for better clarity.
    - Renamed the login email template for consistency and updated the corresponding preview method in the user mailer.

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,10 +1,10 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
 
-  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset_email
-  def password_reset_email
+  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/login_email
+  def login_email
     user = User.order(updated_at: :desc).first
-    UserMailer.password_reset_email(user)
+    UserMailer.login_email(user)
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/new_user_email
